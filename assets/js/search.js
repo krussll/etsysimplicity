b2c8c1d4acabc1783932de2d07833316f21be9a6
krussll <raw987@hotmail.com>
2020-07-08 12:39:49 +0100

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -6,6 +6,7 @@ jQuery(function () {
     console.log('loading data')
   // Wait for the data to load and add it to lunr
   window.data.then(function(loaded_data) {
+    console.log('data loaded')
     console.log(loaded_data)
     var idx = lunr(function () {
       this.field('title');
