25c3ff073ba491eba8ccf1150d92d011be5de2e5
krussll <raw987@hotmail.com>
2020-07-08 12:52:12 +0100

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,8 +2,10 @@ jQuery(function () {
 
   // Get the generated search_data.json file so lunr.js can search it locally.
   window.data = $.getJSON('/search_data.json');
+  const urlParams = new URLSearchParams(window.location.search);
+  const searchQuery = urlParams.get('search');
 
-    console.log('loading data')
+  console.log('loading data')
   // Wait for the data to load and add it to lunr
   window.data.then(function(loaded_data) {
     console.log('data loaded')
@@ -20,11 +22,13 @@ jQuery(function () {
         lunrRef.add(value);
       }, lunrRef);   
     });
-    console.log('doing test search')
-    var results = idx.search('reece'); // Get lunr to perform a search
+    
+    console.log('doing search')
+    var results = idx.search(searchQuery); // Get lunr to perform a search
     console.log(results)
     
     var $search_results = $("#search_results");
+    
     if (results.length) {
         $search_results.empty(); // Clear any old results
 
@@ -43,31 +47,4 @@ jQuery(function () {
         $search_results.html('<li>No results found.<br/>Please check spelling</li>');
       }
   });
-
-  function display_search_results(results) {
-    var $search_results = $("#search_results");
-
-    // Wait for data to load
-    window.data.then(function(loaded_data) {
-
-      // Are there any results?
-      if (results.length) {
-        $search_results.empty(); // Clear any old results
-
-        // Iterate over the results
-        results.forEach(function(result) {
-          var item = loaded_data[result.ref];
-
-          // Build a snippet of HTML for this result
-          var appendString = '<li><a href="' + item.url + '">' + item.title + '</a></li>';
-
-          // Add the snippet to the collection of results.
-          $search_results.append(appendString);
-        });
-      } else {
-        // If there are no results, let the user know.
-        $search_results.html('<li>No results found.<br/>Please check spelling</li>');
-      }
-    });
-  }
 });
